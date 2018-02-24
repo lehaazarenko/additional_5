ba322d63e7759f96a68c4f81f27a5a639997a55e
User <lehaazarenko@mail.ru>
2018-02-24 20:17:00 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	// your solution
+
+	let arr = [], bc = [], stack = [], pos, k = 0;
+	arr = Array.from(str);
+	bc = Array.from(bracketsConfig);
+
+	for (let i = 0; i < arr.length; i++) {
+	  let c = arr[i];
+	  for (let j = 0; j < bc.length; j++) {
+	    if (bc[j].indexOf(c) > -1) {
+	      
+	      pos = j;
+	      stack.push(c); 
+	       
+	      if (bc[pos].indexOf(c) === 1 || bc[pos][0] === bc[pos][1]) {
+	        if (stack[k-1] === bc[pos][0]) {
+	          stack.splice(k-1, 2);
+	          k--;
+	          break;
+	        }
+	      }
+	      
+	      if (i === arr.length - 1) {
+	        i = arr.length + 1;
+	      }
+	     
+	      k++;
+	      break;
+	    }
+	  }
+	}
+	return !stack.length;
 }
